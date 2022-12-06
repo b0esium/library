4f9c13bc8470cfb8d13df08faa728b6c487a25b3
b0esium <b0esium@protonmail.com>
2022-12-06 17:45:44 +0100

    Make add new book function

--- library.js
+++ library.js
@@ -16,6 +16,7 @@ class Book {
 
 let myLibrary = [];
 
+// create a new Book object and add it to the library
 function addToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
@@ -24,6 +25,8 @@ function addToLibrary(title, author, pages, read) {
 let library = document.getElementById("library");
 
 function displayBooks() {
+  // empty previous display
+  emptyLibrary();
   for (book of myLibrary) {
     // create new row
     let newBook = document.createElement("tr");
@@ -47,8 +50,35 @@ function displayBooks() {
   }
 }
 
+function emptyLibrary() {
+  while (library.firstChild) {
+    library.removeChild(library.firstChild);
+  }
+}
+
 // fill with test books
 addToLibrary("Dune", "Herbert", 600, true);
 addToLibrary("Hyperion", "Simmons", 400, true);
 
+// initial display
 displayBooks();
+
+let addBookBtn = document.getElementById("addBookBtn");
+let addBookForm = document.getElementById("addBookForm");
+addBookBtn.addEventListener("click", function () {
+  showForm();
+});
+
+function showForm() {
+  addBookForm.setAttribute("style", "display: block");
+}
+
+addBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let newBookTitle = addBookForm.elements["title"].value;
+  let newBookAuthor = addBookForm.elements["author"].value;
+  let newBookPages = addBookForm.elements["pages"].value;
+  let newBookRead = addBookForm.elements["read"].checked;
+  addToLibrary(newBookTitle, newBookAuthor, newBookPages, newBookRead);
+  displayBooks();
+});
