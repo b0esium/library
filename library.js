928c665905292f4da1430f05977468c1230b113e
b0esium <b0esium@protonmail.com>
2022-12-06 16:37:40 +0100

    Display books

--- library.js
+++ library.js
@@ -14,5 +14,41 @@ class Book {
   }
 }
 
-let dune = new Book("Dune", "Herbert", 400, true);
-dune.info();
+let myLibrary = [];
+
+function addToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+// get table body DOM
+let library = document.getElementById("library");
+
+function displayBooks() {
+  for (book of myLibrary) {
+    // create new row
+    let newBook = document.createElement("tr");
+    library.append(newBook);
+
+    let newTitle = document.createElement("th");
+    newTitle.innerText = book.title;
+    newBook.append(newTitle);
+
+    let newAuthor = document.createElement("th");
+    newAuthor.innerText = book.author;
+    newBook.append(newAuthor);
+
+    let newPages = document.createElement("th");
+    newPages.innerText = book.pages;
+    newBook.append(newPages);
+
+    let newRead = document.createElement("th");
+    newRead.innerText = book.read;
+    newBook.append(newRead);
+  }
+}
+
+// fill with test books
+addToLibrary("Dune", "Herbert", 600, true);
+addToLibrary("Hyperion", "Simmons", 400, true);
+
+displayBooks();
